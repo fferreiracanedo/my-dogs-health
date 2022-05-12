58e9c99ccabc97f731f32b7727788bf5508a0182
anthonifelipi <anthonifelipi@gmail.com>
2022-05-12 12:16:00 -0300

    feat: criação do Footer.

--- mydogshealth/src/App.js
+++ mydogshealth/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { ChakraProvider, Flex } from '@chakra-ui/react';
 import theme from '../src/theme/index';
 import Routes from './routes';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Routes />
-      <Flex height="100vh" backgroundColor="#FED7D7"></Flex>
+      <Flex height="100vh" backgroundColor="#FED7D7">
+        <Footer />
+      </Flex>
     </ChakraProvider>
   );
 }
