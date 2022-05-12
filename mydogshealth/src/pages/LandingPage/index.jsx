4c804295eaed57a6dd273a343da3b1dabf3e26f6
Hyan Lopes <hyanlopess97@gmail.com>
2022-05-11 21:44:15 -0300

    feat: section principal landingPage feita

--- mydogshealth/src/pages/LandingPage/index.jsx
+++ mydogshealth/src/pages/LandingPage/index.jsx
@@ -1,11 +1,9 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
-import SectionPrincipal from '../../components/LandingPageSectionPrincipal';
 const LandingPage = () => {
   return (
     <Flex width="100%">
       <Box width="100%">
         <Text>Landing Page</Text>
-        <SectionPrincipal />
       </Box>
     </Flex>
   );
