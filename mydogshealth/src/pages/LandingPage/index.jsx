21fafa12d2e9108278e1975566eba3902794b699
Hyan Lopes <hyanlopess97@gmail.com>
2022-05-12 13:36:23 -0300

    feat: section secundária criada

--- mydogshealth/src/pages/LandingPage/index.jsx
+++ mydogshealth/src/pages/LandingPage/index.jsx
@@ -1,11 +1,9 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
-import SectionSecundaria from '../../components/LandingPageSectionSecond';
 const LandingPage = () => {
   return (
     <Flex width="100%">
       <Box width="100%">
         <Text>Landing Page</Text>
-        <SectionSecundaria />
       </Box>
     </Flex>
   );
