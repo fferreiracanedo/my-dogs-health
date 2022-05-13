fb43af98fa2ba967b7967060c560f5ba3384ba49
Mateus Ferreira <mateusferreiracanedo@gmail.com>
2022-05-13 09:34:22 -0300

    fix : ajustando regras e css

--- mydogshealth/src/index.js
+++ mydogshealth/src/index.js
@@ -1,17 +1,13 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
 );
-
-
