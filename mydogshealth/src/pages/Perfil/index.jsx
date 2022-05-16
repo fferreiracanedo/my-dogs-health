d722814fc1d6b1bb811fc854d1e449779939d719
Hyan Lopes <hyanlopess97@gmail.com>
2022-05-16 12:37:06 -0300

    feat: Header da pagina de perfil adicionada

--- mydogshealth/src/pages/Perfil/index.jsx
+++ mydogshealth/src/pages/Perfil/index.jsx
@@ -1,5 +1,12 @@
-const Perfil = () => {
-    return (<h1>Perfil</h1>)
-}
+import { Box } from '@chakra-ui/react';
+import HeaderProfile from '../../components/HeaderProfile';
 
-export default Perfil
+const Perfil = () => {
+  return (
+    <Box w="100%" height="100vh">
+      <HeaderProfile></HeaderProfile>
+    </Box>
+  );
+};
+
+export default Perfil;
