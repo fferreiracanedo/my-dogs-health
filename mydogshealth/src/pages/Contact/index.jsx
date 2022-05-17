3d197b3a38b55298d8713e9552894d7a9a4b552d
Carlos Roberto <carlos.rrds@gmail.com>
2022-05-17 15:09:24 -0300

    fixed: concertado quebra na responsividade

--- mydogshealth/src/pages/Contact/index.jsx
+++ mydogshealth/src/pages/Contact/index.jsx
@@ -27,65 +27,72 @@ const Contact = () => {
         backgroundColor="#fff"
         width="100vw"
         minHeight="50vh"
-        alignItems="center"
-        justifyContent="space-around"
-        flexDirection={{ base: 'column', md: 'row' }}
+        justifyContent="center"
       >
-        <Flex marginTop="30px" flexDirection="column" alignItems="center">
-          <AiOutlineMail size="70px" />
-          <Text
-            marginBlock="5px"
-            color="#792323"
-            fontWeight="800"
-            fontSize="32px"
-          >
-            E-mail
-          </Text>
-          <Text marginBlock="5px" fontWeight="600">
-            Tire suas duvidas.
-          </Text>
-          <Text marginBlock="5px" color="#792323" fontWeight="600">
-            ajuda@mydogshelth.com
-          </Text>
-        </Flex>
-        <Flex marginTop="30px" flexDirection="column" alignItems="center">
-          <AiOutlinePhone size="70px" />
-          <Text
-            marginBlock="5px"
-            color="#792323"
-            fontWeight="800"
-            fontSize="32px"
-          >
-            Telefone
-          </Text>
-          <Text marginBlock="5px" fontWeight="600">
-            Ligue a qualquer hora.
-          </Text>
-          <Text marginBlock="5px" color="#792323" fontWeight="600">
-            +55 (41) 99999-9999
-          </Text>
-        </Flex>
         <Flex
-          marginTop="30px"
-          marginBottom={{ base: '30px', md: '0' }}
-          flexDirection="column"
+          width="100vw"
+          maxWidth="1440px"
+          minHeight="50vh"
           alignItems="center"
+          justifyContent="space-around"
+          flexDirection={{ base: 'column', md: 'row' }}
         >
-          <BsChat size="70px" />
-          <Text
-            marginBlock="5px"
-            color="#792323"
-            fontWeight="800"
-            fontSize="32px"
+          <Flex marginTop="30px" flexDirection="column" alignItems="center">
+            <AiOutlineMail size="70px" />
+            <Text
+              marginBlock="5px"
+              color="#792323"
+              fontWeight="800"
+              fontSize="32px"
+            >
+              E-mail
+            </Text>
+            <Text marginBlock="5px" fontWeight="600">
+              Tire suas duvidas.
+            </Text>
+            <Text marginBlock="5px" color="#792323" fontWeight="600">
+              ajuda@mydogshelth.com
+            </Text>
+          </Flex>
+          <Flex marginTop="30px" flexDirection="column" alignItems="center">
+            <AiOutlinePhone size="70px" />
+            <Text
+              marginBlock="5px"
+              color="#792323"
+              fontWeight="800"
+              fontSize="32px"
+            >
+              Telefone
+            </Text>
+            <Text marginBlock="5px" fontWeight="600">
+              Ligue a qualquer hora.
+            </Text>
+            <Text marginBlock="5px" color="#792323" fontWeight="600">
+              +55 (41) 99999-9999
+            </Text>
+          </Flex>
+          <Flex
+            marginTop="30px"
+            marginBottom={{ base: '30px', md: '0' }}
+            flexDirection="column"
+            alignItems="center"
           >
-            Chat
-          </Text>
-          <Text marginBlock="5px" fontWeight="600">
-            Para atendimento imediato.
-          </Text>
-          <Text marginBlock="5px" color="#792323" fontWeight="600">
-            Abra nosso chat online.
-          </Text>
+            <BsChat size="70px" />
+            <Text
+              marginBlock="5px"
+              color="#792323"
+              fontWeight="800"
+              fontSize="32px"
+            >
+              Chat
+            </Text>
+            <Text marginBlock="5px" fontWeight="600">
+              Para atendimento imediato.
+            </Text>
+            <Text marginBlock="5px" color="#792323" fontWeight="600">
+              Abra nosso chat online.
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
