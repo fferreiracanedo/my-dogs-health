657c5a15f8acf7180ccfee2ddec215876231faae
Hyan Lopes <hyanlopess97@gmail.com>
2022-05-19 20:44:39 -0300

    fix: cores do formulario de adicionar informações do dog atualizadas

--- mydogshealth/src/components/FormReport/index.jsx
+++ mydogshealth/src/components/FormReport/index.jsx
@@ -1,84 +1,100 @@
 import {
-    Box,
-    Button,
-    Checkbox,
-    Flex,
-    FormControl,
-    FormLabel,
-    Input,
-    FormErrorIcon,
-    FormErrorMessage,
-    Select,
-    Text,
-  } from '@chakra-ui/react';
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorIcon,
+  FormErrorMessage,
+  Select,
+  Text,
+} from '@chakra-ui/react';
 
-  import {
-    Editable,
-    EditableInput,
-    EditableTextarea,
-    EditablePreview,
-  } from "@chakra-ui/react";
+import {
+  Editable,
+  EditableInput,
+  EditableTextarea,
+  EditablePreview,
+} from '@chakra-ui/react';
 
-  import { useState } from 'react';
-  import * as yup from 'yup';
-  import { useForm } from 'react-hook-form';
-  import { yupResolver } from '@hookform/resolvers/yup';
-  
-  import { useDispatch } from 'react-redux';
-  import { reportRegisterThunk } from '../../store/modules/api/thunks';
-  import { useSelector } from 'react-redux';
-  
-  const FormReport = ({dogId}) => {
-    const loginSchema = yup.object().shape({
-      dogId: yup.string(dogId),
-      action: yup.string().required(),
-      title: yup.string().required(),
-      date: yup.string().required(),
-    });
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      resolver: yupResolver(loginSchema),
-    });
-  
-  
-    const token = useSelector(state => state.user.profile.token);
-    console.log("token",token);
-    const dispatch = useDispatch();
-    const onSubmitFunction = data => {
-      console.log("formReport",token, data.dogId, data.action, data.title, data.notes, data.date);
-        dispatch(reportRegisterThunk(token, data.dogId, data.action, data.title, data.notes, data.date));
-      };
-  
-    
-    return (
-      <Box bgColor="#8E2C2C" padding="20px" borderRadius="15px">
-        <Flex>
-          <form onSubmit={handleSubmit(onSubmitFunction)}>
-            
+import { useState } from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { useDispatch } from 'react-redux';
+import { reportRegisterThunk } from '../../store/modules/api/thunks';
+import { useSelector } from 'react-redux';
+
+const FormReport = ({ dogId }) => {
+  const loginSchema = yup.object().shape({
+    dogId: yup.string(dogId),
+    action: yup.string().required(),
+    title: yup.string().required(),
+    date: yup.string().required(),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const token = useSelector(state => state.user.profile.token);
+  console.log('token', token);
+  const dispatch = useDispatch();
+  const onSubmitFunction = data => {
+    console.log(
+      'formReport',
+      token,
+      data.dogId,
+      data.action,
+      data.title,
+      data.notes,
+      data.date
+    );
+    dispatch(
+      reportRegisterThunk(
+        token,
+        data.dogId,
+        data.action,
+        data.title,
+        data.notes,
+        data.date
+      )
+    );
+  };
+
+  return (
+    <Box bgColor="#2A4058" padding="20px" borderRadius="15px">
+      <Flex>
+        <form onSubmit={handleSubmit(onSubmitFunction)}>
           <FormControl isInvalid={errors.dogId}>
-              <FormLabel color="white" marginTop="5px" htmlFor="dogId">
-                Dog:{' '}
-              </FormLabel>
-              <Input
+            <FormLabel color="white" marginTop="5px" htmlFor="dogId">
+              Dog:
+            </FormLabel>
+            <Input
               value={dogId}
-                placeholder=""
-                borderColor="#855050"
-                id="dogId"
-                {...register('dogId')}
-              />
-              <FormErrorMessage>
-                <FormErrorIcon />
-                {errors.dogId && errors.dogId.message}
-              </FormErrorMessage>
-            </FormControl>
-  
+              placeholder=""
+              borderColor="#fff"
+              id="dogId"
+              {...register('dogId')}
+              bgColor="#fff"
+            />
+            <FormErrorMessage>
+              <FormErrorIcon />
+              {errors.dogId && errors.dogId.message}
+            </FormErrorMessage>
+          </FormControl>
 
-            <FormControl padding="12px" isInvalid={errors.action}>
-            <FormLabel htmlFor="action">Tipo:</FormLabel>
-            <Select {...register('action')} id="action">
+          <FormControl m="15px auto" isInvalid={errors.action}>
+            <FormLabel htmlFor="action" color="white">
+              Tipo:
+            </FormLabel>
+            <Select {...register('action')} id="action" bgColor="#fff">
               <option value="1">Vacina</option>
               <option value="2">Vermífugo</option>
               <option value="3">Nutrição</option>
@@ -95,60 +111,54 @@ import {
             </FormErrorMessage>
           </FormControl>
 
-            <FormControl isInvalid={errors.title}>
-              <FormLabel color="white" marginTop="5px" htmlFor="title">
+          <FormControl isInvalid={errors.title}>
+            <FormLabel color="white" marginTop="5px" htmlFor="title">
               Título:{' '}
-              </FormLabel>
-              <Input
-                placeholder=""
-                borderColor="#855050"
-                id="title"
-                {...register('title')}
-              />
-              <FormErrorMessage>
-                <FormErrorIcon />
-                {errors.title && errors.title.message}
-              </FormErrorMessage>
-            </FormControl>
-            
-  
-            <FormLabel color="white" marginTop="5px" htmlFor="notes">
-              Observação:
             </FormLabel>
-
-            <Editable defaultValue='Efeitos e Médico Veterinário responsável.'>
-            <EditablePreview />
-            <EditableTextarea
-                as={'textarea'}
-                placeholder=""
-                {...register('notes')}
-                height="80px"
+            <Input
+              placeholder=""
+              borderColor="#855050"
+              id="title"
+              {...register('title')}
+              bgColor="#fff"
             />
-            </Editable>
+            <FormErrorMessage>
+              <FormErrorIcon />
+              {errors.title && errors.title.message}
+            </FormErrorMessage>
+          </FormControl>
 
-  
-           <FormControl padding="12px" isInvalid={errors.date}>
-            <FormLabel>Data:</FormLabel>
+          <FormLabel color="white" marginTop="5px" htmlFor="notes">
+            Observação:
+          </FormLabel>
+          <Input
+            as={'textarea'}
+            placeholder=""
+            {...register('notes')}
+            height="80px"
+            bgColor="#fff"
+          ></Input>
+
+          <FormControl isInvalid={errors.date} mb="20px">
+            <FormLabel color="white">Data:</FormLabel>
             <Input
               type="date"
               borderColor="#855050"
-              width="100%"
               placeholder=""
               {...register('date')}
+              bgColor="#fff"
             />
             <FormErrorMessage>
               <FormErrorIcon />
               {errors.date && errors.date.message}
             </FormErrorMessage>
-            </FormControl>
-
-            <Button type="submit" width="100%" bgColor="#FED7D7">
-              Enviar
-            </Button>
-          </form>
-        </Flex>
-      </Box>
-    );
-  };
-  export default FormReport;
-  
+          </FormControl>
+          <Button type="submit" width="100%" bgColor="#fff">
+            Enviar
+          </Button>
+        </form>
+      </Flex>
+    </Box>
+  );
+};
+export default FormReport;
