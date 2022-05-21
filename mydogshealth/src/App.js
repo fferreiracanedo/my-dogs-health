b4fa85bd1f1102eddbeaac9d640bfaf561e46b25
Mateus Ferreira Canedo <mateusferreiracanedo@gmail.com>
2022-05-21 13:17:20 -0300

    refactor:Ajustando imagens e alinhamento

--- mydogshealth/src/App.js
+++ mydogshealth/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Box } from '@chakra-ui/react';
 import theme from '../src/theme/index';
 import Routes from './routes';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Flex
-        width="100%"
-        justifyContent="center"
-        bgColor="#2A4058"
-        color="#FFF"
-      >
+      <Flex width="100%" bgColor="#2A4058" color="#FFF">
         <Routes />
       </Flex>
     </ChakraProvider>
